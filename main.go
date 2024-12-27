1622600ef752a0e9caea5986fbeaecec84e0b698
tao73bot <tawhidulbali00@gmail.com>
2024-12-27 13:16:32 +0600

    Add Swagger documentation setup with initial configuration and dependencies

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"github.com/tao73bot/A_simple_CRM/initializers"
 	"github.com/tao73bot/A_simple_CRM/routes"
 )
@@ -14,14 +17,31 @@ func init() {
 	initializers.Migrate()
 }
 
+// @BasePath /api/v1
+
+// PingExample godoc
+// @Summary ping example
+// @Schemes
+// @Description do ping
+// @Tags example
+// @Accept json
+// @Produce json
+// @Success 200 {string} Helloworld
+// @Router /example/helloworld [get]
+
 func main() {
 	fmt.Println("Starting the CRM application ...")
 
 	r := gin.Default()
-	routes.AuthRoutes(r)
+	r.StaticFS("/docs", http.Dir("./docs"))
+	r.GET("swagger/*any", ginSwagger.CustomWrapHandler(
+		&ginSwagger.Config{
+			URL: "/docs/swagger.yaml",
+		}, swaggerFiles.Handler))
 	routes.UserRoutes(r)
 	routes.LeadRoutes(r)
 	routes.CustomerRoutes(r)
 	routes.InteractionRoutes(r)
+	routes.AuthRoutes(r)
 	r.Run()
 }
